14610788814c956566106ce4d7f9007fefb0c41a
Yonghwan SO <sio4@sk.com>
2013-02-27 19:18:21 +0900

    rails new sosmon --skip-bundle

---
+++ sosmon/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sosmon::Application.config.secret_token = '113bfec603c9e8743dcaeae0157b3f78ef6fe0fbdeb50fb6e1087bc0aeef7374e95d56cdf860e857ba7cd1808125572649b4fd6bc9b531f47e0f57159ab07a21'
